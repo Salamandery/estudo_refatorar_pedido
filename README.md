d17dbb4f2fda78186672efc2864457c1819d5d30
salamandery <sp.ront@hotmail.com>
2021-09-21 09:50:52 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+## Estudo de TDD e Refatoração
+
+### Teste de sistema de vendas online com a possibilidade de realizar pedidos com múltiplos itens, cada um deles com uma quantidade variável, calculando o frete, os impostos, aplicando um cupom de desconto e ainda interagindo com o estoque. 
+
+-  Não deve fazer um pedido com cpf inválido 
+-  Deve fazer um pedido com 3 itens (com descrição, preço e quantidade) 
+-  Deve fazer um pedido com cupom de desconto (percentual sobre o total do pedido) 
+
+Rodolfo M F Abreu
---
+++ dist/validateCPF.js
@@ -0,0 +1,46 @@
+"use strict";
+const FACTOR_DIGIT_1 = 10;
+const FACTOR_DIGIT_2 = 11;
+const MAX_DIGITS_1 = 9;
+const MAX_DIGITS_2 = 10;
+function isInvalidLength(cpf) {
+    return (cpf.length < 11 || cpf.length > 14);
+}
+function stringTreatement(cpf) {
+    return cpf.replace(/\D/g, "");
+}
+function stringToNumberArray(cpf) {
+    return [...cpf].map(digit => parseInt(digit));
+}
+function ifAllNumberAreEquals(cpf) {
+    const [digit1] = cpf;
+    return cpf.split("").every(c => c === digit1);
+}
+function getCheckDigit(cpf) {
+    return cpf.slice(9);
+}
+function calculateDigit(cpf, factor, max) {
+    let total = 0;
+    for (const digit of stringToNumberArray(cpf).slice(0, max)) {
+        total += digit * factor--;
+    }
+    return (total % 11 < 2) ? 0 : (11 - total % 11);
+}
+function validateCPF(cpf) {
+    if (cpf === null)
+        return false;
+    if (cpf === undefined)
+        return false;
+    cpf = stringTreatement(cpf);
+    if (isInvalidLength(cpf))
+        return false;
+    if (ifAllNumberAreEquals(cpf))
+        return false;
+    const digit1 = calculateDigit(cpf, FACTOR_DIGIT_1, MAX_DIGITS_1);
+    const digit2 = calculateDigit(cpf, FACTOR_DIGIT_2, MAX_DIGITS_2);
+    const cpfCalculated = `${digit1}${digit2}`;
+    return cpfCalculated == getCheckDigit(cpf);
+}
+console.log(validateCPF("111.111.111-11"));
+console.log(validateCPF("123.456.789-99"));
+console.log(validateCPF("935.411.347-80"));
